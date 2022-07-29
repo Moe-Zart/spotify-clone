f75cd91ca61b86916d655995f8750c49e62f5004
Moe-Zart <mohamedzreika03@outlook.com>
2022-07-29 20:43:04 +1000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Nav from "./components/nav";
-import Header from "./components/header";
-import Boxes from "./components/4 boxes";
+
 function App() {
   return (
-    <div className="nav-header">
-      <Nav />
-      <Header />
-      <Boxes />
+    <div>
+      
     </div>
   );
 }
